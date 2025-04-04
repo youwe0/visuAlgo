0853a3d93c85542fd0e148824406ef74970c1b71
youwe0 <yuvrajaingh1508@gmail.com>
2025-04-04 17:22:14 +0530

    change to tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    
-    <div>hii i am project --------VISUALGO ---------------</div>
+
+    <div className='bg-red-400 rounded-md grid place-content-center  items-center h-15 m-4'>--------------    PROJECT VISUALGO    ---------------</div>
   )
 }
 
